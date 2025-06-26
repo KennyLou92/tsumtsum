3cdd35337d9ae2e0e4a8e0a0d33b1d05f57575a0
KennyLou92 <114062621+KennyLou92@users.noreply.github.com>
2025-06-26 17:06:29 +0800

    Update littletsum.js

--- littletsum.js
+++ littletsum.js
@@ -1,5 +1,11 @@
 const littletsumData = [
 { "^508w^" : "block_woodypr_1_l.png" },
 { "^509w^" : "block_woodypr_2_l.png" },
+{"554w" : "block_bellepr_1_l.png.png"},
+{"555w" : "block_bellepr_2_l.png.png"},
+{ "^441w^" : "block_mikepr_1_l.png"},
+{ "782w" : "block_starwarsset_2_l.png"},
+{ "783w" : "block_starwarsset_3_l.png"},
+{ "784w" : "block_starwarsset_4_l.png"},
 ];
 window.littletsumData = littletsumData;
