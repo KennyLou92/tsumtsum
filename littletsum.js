e4e8763a4dbe775dd6564ad96afa0ebf39d23718
KennyLou92 <114062621+KennyLou92@users.noreply.github.com>
2025-06-26 09:12:21 +0800

    Update littletsum.js

--- littletsum.js
+++ littletsum.js
@@ -1,5 +1,5 @@
 const littletsumData = [
-{ ^508w^ : "block_woodypr_1_l.png" },
-{ ^509w^ : "block_woodypr_2_l.png" },
+{ "^508w^" : "block_woodypr_1_l.png" },
+{ "^509w^" : "block_woodypr_2_l.png" },
 ];
 window.littletsumData = littletsumData;
