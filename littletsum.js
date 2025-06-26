bccc3475a2568748121d0574945efe52b9030086
KennyLou92 <114062621+KennyLou92@users.noreply.github.com>
2025-06-26 17:11:53 +0800

    Update littletsum.js

--- littletsum.js
+++ littletsum.js
@@ -1,8 +1,8 @@
 const littletsumData = [
 { "^508w^" : "block_woodypr_1_l.png" },
 { "^509w^" : "block_woodypr_2_l.png" },
-{"554w" : "block_bellepr_1_l.png.png"},
-{"555w" : "block_bellepr_2_l.png.png"},
+{"554w" : "block_bellepr_1_l.png"},
+{"555w" : "block_bellepr_2_l.png"},
 { "^441w^" : "block_mikepr_1_l.png"},
 { "782w" : "block_starwarsset_2_l.png"},
 { "783w" : "block_starwarsset_3_l.png"},
