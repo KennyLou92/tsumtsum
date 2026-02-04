c5999d1ecbe339c8fc57ecee428d315571161674
KennyLou92 <114062621+KennyLou92@users.noreply.github.com>
2026-02-04 09:35:24 +0800

    Update box_tsum values in selectbox.js

--- selectbox.js
+++ selectbox.js
@@ -405,7 +405,7 @@ const selectbox = [
 		box:"feb26selectbox2",
 		show:1,
 		box_label:"https://game-lgtmtmg.line-scdn.net/COMMON/G20260245gy67jhgedw/images/event_banner/banner_gl_0793_tw.png",
-		box_tsum:[580,477,443,402,539,261,121,349,400,43],
+		box_tsum:[580,477,443,402,539,261,283,147,121,349,400,43],
 		startdate:1771488000,
 		enddate:1771833599,
 	},
